ab073cc5361ae39025cb5d5091915950d81abb09
sachith-1 <cst17043@std.uwu.ac.lk>
2020-03-19 17:01:27 +0530

    mainApp.js,neu.css created App.js modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class reactComponents extends Component {
+  state = {
+    spanStyles: {
+      margin: 10
+    },
+    pStyles: {
+      visibility: "hidden"
+    }
+  };
+  render() {
+    return (
+      <div>
+        <input type="button" value="Get Ram Usage" onClick={this.ramUsage} />
+        <p id="ram" style={this.state.pStyles}>
+          Available Memory :
+          <span id="aMem" style={this.state.spanStyles}></span>
+          Total Memory :<span id="tMem" style={this.state.spanStyles}></span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+  ramUsage() {
+    Neutralino.computer.getRamUsage(
+      function(data) {
+        document.getElementById("aMem").innerHTML =
+          (data["ram"]["available"] / (1024 * 1024)).toFixed(3) + " GB";
+        document.getElementById("tMem").innerHTML =
+          (data["ram"]["total"] / (1024 * 1024)).toFixed(3) + " GB";
+      },
+      function() {
+        document.getElementById("aMem").innerHTML = "Error While Executing";
+      }
+    );
+    document.getElementById("ram").style.visibility = "visible";
+  }
 }
 
-export default App;
+export default reactComponents;
