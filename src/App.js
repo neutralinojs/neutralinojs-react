06212ac018b765c22b143a5b13c82e70165c4a01
sachith-1 <cst17043@std.uwu.ac.lk>
2020-04-06 13:17:24 +0530

    Ram Usage Example into Separate Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from "react";
+import RamUsageExample from "./RamUsageExample ";
 
 class reactComponents extends Component {
   state = {
     aMemVal: "",
     tMemVal: "",
-    spanStyles: {
-      margin: 10
-    },
-    isbtnRamUsage: 0,
-    btnRamUsage: {
-      color: " #fff",
-      backgroundColor: "#28a745",
-      borderColor: "#28a745",
-      borderRadius: "0.25rem",
-      fontWeight: 400,
-      textAlign: "center",
-      border: "1px solid transparent",
-      padding: ".375rem .75rem",
-      fontSize: " 1rem",
-      lineHeight: "1.5",
-      cursor: "pointer"
-    }
   };
   render() {
     return (
@@ -30,59 +14,14 @@ class reactComponents extends Component {
           <br />
           <span>v {NL_VERSION} </span>
         </h3>
+        <div id="ramUsage"></div>
+        {}
         {/* NeutralinoJs example for get current available and total ram in Gb*
-          remove comment for below line/}
-        {/*this.GetMemBtn()*/}
-        {this.state.isbtnRamUsage === 1 ? (
-          <p id="ram">
-            Available Memory :
-            <span id="aMem" style={this.state.spanStyles}>
-              {this.state.aMemVal}
-            </span>
-            Total Memory :
-            <span id="tMem" style={this.state.spanStyles}>
-              {this.state.tMemVal}
-            </span>
-          </p>
-        ) : (
-          ""
-        )}
+        remove comment for below line*/}
+        {/* <RamUsageExample/> */}
       </div>
     );
   }
-  GetMemBtn() {
-    return (
-      <input
-        type="button"
-        style={this.state.btnRamUsage}
-        value="Get Ram Usage"
-        onClick={() => {
-          this.ramUsage();
-          this.isRamBtnClicked();
-        }}
-      />
-    );
-  }
-
-  ramUsage() {
-    Neutralino.computer.getRamUsage(
-      function(data) {
-        let aMem =
-          (data["ram"]["available"] / (1024 * 1024)).toFixed(3) + " GB";
-        this.setState({ aMemVal: aMem });
-
-        let tMem = (data["ram"]["total"] / (1024 * 1024)).toFixed(3) + " GB";
-        this.setState({ tMemVal: tMem });
-      }.bind(this),
-      function() {
-        this.setState({ aMemVal: "Error While Executing" });
-      }.bind(this)
-    );
-  }
-
-  isRamBtnClicked = () => {
-    this.setState({ isbtnRamUsage: 1 });
-  };
 }
 
 export default reactComponents;
---
+++ src/RamUsageExample .js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+class RamUsageExample extends Component {
+  state = {
+    spanStyles: {
+      margin: 10,
+    },
+    isbtnRamUsage: 0,
+    btnRamUsage: {
+      color: " #fff",
+      backgroundColor: "#28a745",
+      borderColor: "#28a745",
+      borderRadius: "0.25rem",
+      fontWeight: 400,
+      textAlign: "center",
+      border: "1px solid transparent",
+      padding: ".375rem .75rem",
+      fontSize: " 1rem",
+      lineHeight: "1.5",
+      cursor: "pointer",
+    },
+  };
+  render() {
+    return (
+      <div>
+        {this.GetMemBtn()}
+        {this.state.isbtnRamUsage === 1 ? (
+          <p id="ram">
+            Available Memory :
+            <span id="aMem" style={this.state.spanStyles}>
+              {this.state.aMemVal}
+            </span>
+            Total Memory :
+            <span id="tMem" style={this.state.spanStyles}>
+              {this.state.tMemVal}
+            </span>
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+
+  GetMemBtn() {
+    return (
+      <input
+        type="button"
+        style={this.state.btnRamUsage}
+        value="Get Ram Usage"
+        onClick={() => {
+          this.ramUsage();
+          this.isRamBtnClicked();
+        }}
+      />
+    );
+  }
+
+  ramUsage() {
+    Neutralino.computer.getRamUsage(
+      function (data) {
+        let aMem =
+          (data["ram"]["available"] / (1024 * 1024)).toFixed(3) + " GB";
+        this.setState({ aMemVal: aMem });
+
+        let tMem = (data["ram"]["total"] / (1024 * 1024)).toFixed(3) + " GB";
+        this.setState({ tMemVal: tMem });
+      }.bind(this),
+      function () {
+        this.setState({ aMemVal: "Error While Executing" });
+      }.bind(this)
+    );
+  }
+
+  isRamBtnClicked = () => {
+    this.setState({ isbtnRamUsage: 1 });
+  };
+}
+
+export default RamUsageExample;
